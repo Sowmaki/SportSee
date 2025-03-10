64dd1b7435f1dd798d363a39d455bddaa316e17f
sowmaki <solene.tassel@hotmail.com>
2025-03-10 22:48:28 +0100

    RadarChart performances terminé

--- FRONTEND/SportSeeApp/src/components/Performance/Performance.jsx
+++ FRONTEND/SportSeeApp/src/components/Performance/Performance.jsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react"
 import { useParams } from "react-router-dom"
+import { PolarAngleAxis, PolarGrid, PolarRadiusAxis, Radar, RadarChart } from "recharts"
 import { useUsersPerformance } from "../../hooks/useUsersPerformance"
 import Erreur from "../../pages/Erreur/Erreur"
 
@@ -23,12 +24,51 @@ const Performance = () => {
   if (error) return error
 
   if (!userPerformance) return <Erreur />
-  const performance1 = `Jour ${userPerformance.kind[1]}: ${userPerformance.data[0].value}`
+
+  const formatKindOfPerformances = (userPerformance) => {
+
+    const kindTranslation = {
+      cardio: "Cardio",
+      energy: "Energie",
+      endurance: "Endurance",
+      strength: "Force",
+      speed: "Vitesse",
+      intensity: "Intensité"
+    };
+
+    const reversedData = [...userPerformance.data].reverse();
+
+    return {
+      ...userPerformance,
+      data: reversedData.map(session => ({
+        ...session,
+        kind: kindTranslation[userPerformance.kind[session.kind]]
+      }))
+    }
+  }
+
+  const formattedPerformances = formatKindOfPerformances(userPerformance)
 
   return (
     <>
-      {performance1}
-
+      <RadarChart cx="50%" cy="50%" outerRadius={70} width={258} height={263} data={formattedPerformances.data} style={{ backgroundColor: '#282D30', borderRadius: '5px' }}>
+        <PolarGrid radialLines={false} polarRadius={[0, 9, 18, 37, 56, 75]} />
+        <PolarAngleAxis
+          dataKey="kind"
+          type="category"
+          style={{ fontSize: '0.75rem' }}
+          tickLine={false}
+          tickSize={25}
+          tick={{ fill: "white", dy: 5, dx: 0 }}
+          outerRadius={10}
+        />
+        <PolarRadiusAxis
+          domain={[0, 100]}
+          tick={false}
+          axisLine={false}
+        />
+        <Radar dataKey="value" fill="#FF0101" fillOpacity={0.6} />
+      </RadarChart>
     </>
   )
 
